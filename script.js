597e86ed2101d82946065d4f42248c10777427bf
cspoppuppy <cspoppuppy@hotmail.com>
2020-12-31 00:28:08 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //const headImg=document.getElementById("headImg");
 //headImg.width=window.innerWidth;
+function responsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
